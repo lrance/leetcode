68e4075ff332b20f1945b7399d0e89b9a02ece2b
lrance <hanzhelee@gmail.com>
2019-02-23 23:14:48 -0500

    Update populating_next_right_pointers_in_each_node.java
    You may only use constant extra space.

--- populating_next_right_pointers_in_each_node.java
+++ populating_next_right_pointers_in_each_node.java
@@ -18,6 +18,22 @@ class Node {
 */
 class Solution {
     public Node connect(Node root) {
-        
+        // space O(1)
+        if(root == null) return root;
+        Node start = root;
+        while(start != null) {
+            Node cur = start;
+            while(cur != null) {
+                if(cur.left != null) {
+                    cur.left.next = cur.right;
+                }
+                if(cur.right != null && cur.next != null) {
+                    cur.right.next = cur.next.left;
+                }
+                cur = cur.next;
+            }
+            start = start.left;
+        }
+        return root;
     }
 }
